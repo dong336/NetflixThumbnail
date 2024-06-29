0ab301d1dd65a3bb4da2f9e60a8ccb119ea41211
kdw <50583568+dong336@users.noreply.github.com>
2024-06-29 22:16:59 +0900

    Initial Commit

---
+++ NetflixThumbnail/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NetflixThumbnail
+//
+//  Created by 김동운 on 6/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NetflixThumbnail/NetflixThumbnailApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixThumbnailApp.swift
+//  NetflixThumbnail
+//
+//  Created by 김동운 on 6/29/24.
+//
+
+import SwiftUI
+
+@main
+struct NetflixThumbnailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
